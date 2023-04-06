c89ce95e61580b7a955671f537a6264172f2c396
Samuel <sixgod666@gmail.com>
2023-04-06 10:42:24 -0500

    version2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tag, Icon } from "./MyTabsComponent.js";
+import "./App.css"
 
+const ICON = "https://w7.pngwing.com/pngs/336/860/png-transparent-shiba-inu-dogecoin-cryptocurrency-wallet-doge-mammal-carnivoran-dog-like-mammal.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Tag>
+        <div title={"Section title 1"}>Content of section 1</div>
+      </Tag>
+      <Tag color="purple" borderRadius="100px">
+        <div title={"Section title 2"}>Content</div>
+      </Tag>
+
+      <Icon src={ICON}></Icon>
+
+      <Tag>
+        <div title={"Icon inside tag"}>
+          <Icon src={ICON}></Icon>
+          Content
+        </div>
+      </Tag>
     </div>
   );
 }
