fcd43663b4e92a209ca3dbd9dcf8f1ee0b69d30a
Samuel <sixgod666@gmail.com>
2023-04-06 11:01:57 -0500

    meet requirement

--- src/MyTabsComponent.js
+++ src/MyTabsComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 
 const IconContext = React.createContext(false);
@@ -20,54 +20,52 @@ const ContentWrapper = styled.div`
 `;
 
 const Tag = ({ children, color, borderRadius }) => {
-  return (
-    <TagWrapper>
-      <IconContext.Provider value={true}>
-        {React.Children.map(children, (child) => {
-          if (child.props.title) {
-            return (
-              <>
-                <Title>{child.props.title}</Title>
-                <ContentWrapper color={color} borderRadius={borderRadius}>
-                  {child.props.children}
-                </ContentWrapper>
-              </>
-            );
-          }
-          return child;
-        })}
-      </IconContext.Provider>
-    </TagWrapper>
-  );
+    return (
+        <TagWrapper>
+            <IconContext.Provider value={true}>
+                {React.Children.map(children, (child) => {
+                    if (child.props.title) {
+                        return (
+                            <>
+                                <Title>{child.props.title}</Title>
+                                <ContentWrapper color={color} borderRadius={borderRadius}>
+                                    {child.props.children}
+                                </ContentWrapper>
+                            </>
+                        );
+                    }
+                    return child;
+                })}
+            </IconContext.Provider>
+        </TagWrapper>
+    );
 };
 
 const Icon = ({ src }) => {
-  return (
-    <IconContext.Consumer>
-      {(insideTag) => (
+    const insideTag = useContext(IconContext);
+    return (
         <StyledIcon
-          src={src}
-          insideTag={insideTag}
-          onMouseEnter={(e) => {
-            if (!insideTag) e.target.style.filter = "blur(0)";
-          }}
-          onMouseLeave={(e) => {
-            if (!insideTag) e.target.style.filter = "blur(6px)";
-          }}
+            src={src}
+            insideTag={insideTag}
+            onMouseEnter={(e) => {
+                if (insideTag) e.target.style.filter = "blur(0)";
+            }}
+            onMouseLeave={(e) => {
+                if (insideTag) e.target.style.filter = "blur(6px)";
+            }}
         />
-      )}
-    </IconContext.Consumer>
-  );
+    );
 };
 
 const StyledIcon = styled.img`
-  filter: blur(6px);
+  /* filter: ${(props) => (props.insideTag ? "blur(6px)" : "blur(0)")}; */
+  filter: ${(props) => "blur(6px)"};
   width: 32px;
   height: 32px;
 
   ${(props) =>
-    props.insideTag &&
-    `
+        props.insideTag &&
+        `
     &:hover {
       filter: blur(0);
     }
